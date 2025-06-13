e6b3398a3876c259b3e3d798a337f6c099f67e3b
bender2352 <165169475+bender2352@users.noreply.github.com>
2025-06-13 22:40:21 +0200

    setup wizard

--- packages/probot/lib/helpers/webhook-proxy.js
+++ packages/probot/lib/helpers/webhook-proxy.js
@@ -1,20 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.createWebhookProxy = void 0;
-const createWebhookProxy = (opts) => {
-    try {
-        const SmeeClient = require("smee-client");
-        const smee = new SmeeClient({
-            logger: opts.logger,
-            source: opts.url,
-            target: `http://localhost:${opts.port}${opts.path}`,
-        });
-        return smee.start();
-    }
-    catch (error) {
-        opts.logger.warn("Run `npm install --save-dev smee-client` to proxy webhooks to localhost.");
-        return;
-    }
+
+// Minimal webhook proxy stub for setup app
+const createWebhookProxy = () => {
+    return {
+        close: () => {},
+        url: null
+    };
 };
 exports.createWebhookProxy = createWebhookProxy;
-//# sourceMappingURL=webhook-proxy.js.map
